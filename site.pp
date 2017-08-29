2783b20092093a99b4048d5cbff5d61e4e84c465
Pedro Cravo Lopes <pedro.clopes@feddy.lopes.local>
2017-08-29 14:24:37 +0100

    ensure and target added to class host_entries

--- site.pp
+++ site.pp
@@ -31,18 +31,24 @@ Qualquer coisa...
 class host_entries {
 
 	host { 'foreman.redhat.local':
+		ensure			=> 'present',
 		ip				=> '192.168.122.51',
 		host_aliases	=> 'foreman',
+		target			=> '/etc/hosts',
 	} # end host foreman.redhat.local
 
 	host { 'puppetmaster.redhat.local':
+		ensure			=> 'present',
 		ip				=> '192.168.122.55',
 		host_aliases	=> 'puppetmaster',
+		target			=> '/etc/hosts',
 	} # end host puppetmaster.redhat.local
 
 	host { 'puppetmaster2.redhat.local':
+		ensure			=> 'present',	
 		ip				=> '192.168.122.54',
 		host_aliases	=> 'puppetmaster2',
+		target			=> '/etc/hosts',
 	} # end host puppetmaster.redhat.local
 
 } # end host_entries CLASS
